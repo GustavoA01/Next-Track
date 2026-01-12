f9a8cc922b37d9735c34f673238622c7d8efad4d
GustavoA01 <gustavoaguiar14325@gmail.com>
2026-01-12 20:26:23 -0300

    icone de adicionado

--- src/features/MusicCard/components/MusicInfo.tsx
+++ src/features/MusicCard/components/MusicInfo.tsx
@@ -3,11 +3,9 @@ type MusicInfoProps = {
   artistName: string;
 };
 
-export const MusicInfo = ({ musicName, artistName }: MusicInfoProps) => {
-  return (
-    <div className="flex flex-col ">
-      <p className="text-lg font-semibold line-clamp-2">{musicName}</p>
-      <p className="text-sm text-muted-foreground line-clamp-1">{artistName}</p>
-    </div>
-  );
-};
+export const MusicInfo = ({ musicName, artistName }: MusicInfoProps) => (
+  <div className="flex flex-col ">
+    <p className="text-lg font-semibold line-clamp-2">{musicName}</p>
+    <p className="text-sm text-muted-foreground line-clamp-1">{artistName}</p>
+  </div>
+);
--- src/features/MusicCard/components/RightInfo.tsx
+++ src/features/MusicCard/components/RightInfo.tsx
@@ -1,4 +1,6 @@
-import { Plus } from "lucide-react";
+import { Check, Plus } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 
 type RightInfoProps = {
   duration: string;
@@ -6,14 +8,30 @@ type RightInfoProps = {
 };
 
 export const RightInfo = ({ duration, onAddToPlaylist }: RightInfoProps) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleCLick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (isClicked) {
+      toast.info("Música já adicionada à playlist");
+      return;
+    }
+    setIsClicked(true);
+    onAddToPlaylist(e);
+  };
+
   return (
     <div className="flex items-center gap-2">
       <p className="text-sm text-muted-foreground">{duration}</p>
       <div
-        onClick={onAddToPlaylist}
-        className="group/add hover:bg-primary border border-primary transition-all duration-200 rounded-full p-2"
+        onClick={(e) => handleCLick(e)}
+        className={`border border-primary transition-all duration-200 rounded-full p-2 
+        ${isClicked ? "bg-primary" : "group/add hover:bg-primary"}`}
       >
-        <Plus className="md:w-6 md:h-6 w-4 h-4 text-white m-auto group-hover/add:text-black" />
+        {isClicked ? (
+          <Check className="animate-scale-appear text-black m-auto md:w-6 md:h-6 w-4 h-4" />
+        ) : (
+          <Plus className="md:w-6 md:h-6 w-4 h-4 text-white m-auto group-hover/add:text-black" />
+        )}
       </div>
     </div>
   );
